463dfa6c9fb8f5ae7fb821b26e4be6b375e6e1a4
Jesus Garcia <jesus.garcia@iukanet.com>
2025-07-08 08:25:31 +0200

    js cambiado

--- cleanUp.js
+++ cleanUp.js
@@ -1,19 +1,10 @@
-// cleanUp.js
 const admin = require("firebase-admin");
 
+// Leer y parsear el JSON completo desde la variable FIREBASE_CONFIG_JSON
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
-    client_x509_cert_url: process.env.CLIENT_CERT,
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
@@ -23,7 +14,6 @@ const db = admin.firestore();
   const expiracion = 1000 * 60 * 30; // 30 minutos
 
   const snapshot = await db.collection('salas').get();
-
   let eliminadas = 0;
 
   for (const doc of snapshot.docs) {
